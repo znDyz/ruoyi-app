78bdb0414dd08624fae8aabd70b7dbf8f65da270
dongyunzhao <32562424@qq.com>
2020-03-22 21:13:36 +0800

    修改聊天页面

--- myFirstApp/pages/paper/paper.vue
+++ myFirstApp/pages/paper/paper.vue
@@ -6,12 +6,41 @@
 			@addFriends="addFriends"
 			@clearCache="clearCache"
 			></paper-left-popup>
-		<!--小纸条列表-->
+		<!--
+		小纸条列表
 		<block v-for="(item,index) in list" :key="index">
 			<paper-list :item="item" :index="index"></paper-list>
 		</block>
-		<!--上拉加载-->
+		上拉加载
 		<load-more :loadtext="loadtext"></load-more>
+		-->
+		<swiper-tab-head
+			:tabBars="tabBars" 
+			:tabIndex="tabIndex"
+			@tabCentChange="tabChange">
+		</swiper-tab-head>
+		<view class="uni-tab-bar">
+			<swiper class="swiper-box" @change="slideChange"
+					:style="{height:swiperheight+'px'}" :current="tabIndex">
+				<swiper-item v-for="(items,index) in mylist" :key="index">
+					<scroll-view scroll-y @scrolltolower="loadmore(index)" style="height:100%;">
+						<template v-if="items.list.length>0">
+							<block v-for="(item, index1) in items.list" :key="index1">
+								<paper-list :item="item" :index="index"></paper-list>
+							</block> 
+							<!--上拉加载组件-->
+							<load-more :loadtext="items.loadtext"></load-more>
+						</template>
+						<template v-else>
+							<!--无内容默认组件-->
+							<noThing></noThing>
+						</template>
+					</scroll-view>
+				</swiper-item>
+			</swiper>
+		</view>
+		
+		
 	</view>
 </template>
 
@@ -19,68 +48,75 @@
 	import paperList from "../../components/paper/paper-list.vue";
 	import loadMore from "../../components/common/load-more.vue";
 	import paperLeftPopup from "../../components/paper/paper-left-popup.vue";
+	import swiperTabHead from "../../components/index/swiper-tab-head.vue";
+	import noThing from "../../components/common/no-thing.vue";
 	export default {
 		components: {
 			paperList,
 			loadMore,
-			paperLeftPopup
+			paperLeftPopup,
+			swiperTabHead,
+			noThing
 		},
 		data() {
 			return {
+				swiperheight:500,
+				tabIndex:0,
+				tabBars: [
+					{name: '我的好友',id: 'haoyou'}, 
+					{name: '我的群组',id: 'qunzu'}
+				],
 				popupShow:false,
 				loadtext: "上拉加载更多...",
-				list:[
+				mylist:[
 					{
-						userpic:"../../static/demo/userpic/12.jpg",
-						username:"昵称一号",
-						time:"10:21",
-						data:"我是第一条信息",
-						noreadnum:10
+						loadtext:"上拉加载更多...",
+						list:[
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称一号",
+								time:"10:21",
+								data:"我是第一条信息",
+								noreadnum:"10"
+							}
+						]
 					},
 					{
-						userpic:"../../static/demo/userpic/12.jpg",
-						username:"昵称二号",
-						time:"10:21",
-						data:"我是第二条信息",
-						noreadnum:0
-					},
-					{
-						userpic:"../../static/demo/userpic/12.jpg",
-						username:"昵称三号",
-						time:"10:21",
-						data:"我是第三条信息",
-						noreadnum:8
-					},
-					{
-						userpic:"../../static/demo/userpic/12.jpg",
-						username:"昵称三号",
-						time:"10:21",
-						data:"我是第三条信息",
-						noreadnum:8
-					},
-					{
-						userpic:"../../static/demo/userpic/12.jpg",
-						username:"昵称三号",
-						time:"10:21",
-						data:"我是第三条信息",
-						noreadnum:8
-					},
-					{
-						userpic:"../../static/demo/userpic/12.jpg",
-						username:"昵称三号",
-						time:"10:21",
-						data:"我是第三条信息",
-						noreadnum:8
-					},
-					{
-						userpic:"../../static/demo/userpic/12.jpg",
-						username:"昵称三号",
-						time:"10:21",
-						data:"我是第三条信息",
-						noreadnum:8
+						loadtext:"上拉加载更多...",
+						list:[
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"群组一号",
+								time:"10:21",
+								data:"我是第一条信息",
+								noreadnum:8
+							}
+						]
 					}
 				]
 			}
+		},
+		onLoad(){
+			let _this = this;
+			uni.getSystemInfo({
+			    success:(res)=> {
+			        let height = res.screenHeight-uni.upx2px(100);
+			        _this.swiperheight = height;
+			    }
+			});
+			//加载好友信息
+			this.iGlobal.request({
+			    url:'/queryFriends', method:'GPOST',
+				data:{
+					userid:"15619232673"
+				}
+			}).then((res)=>{
+				console.log(JSON.stringify(res));
+			   
+			}).catch((err)=>{
+				console.log("请求失败"+JSON.stringify(res));
+			});
+			
 		},
 		//页面滚动到底部的事件(不是scroll-view滚到底)常用于下拉下一页数据
 		onReachBottom(){
@@ -104,19 +140,13 @@
 		},
 		methods: {
 			addFriends(){
-				console.log("添加好友");
-				this.hidePopup();
+				console.log("添加好友");this.hidePopup();
 			},
 			clearCache(){
-				console.log("清除缓存");
-				this.hidePopup();
+				console.log("清除缓存");this.hidePopup();
 			},
-			showPopup(){
-				this.popupShow=true;	//显示菜单
-			},
-			hidePopup(){	
-				this.popupShow=false;	//隐藏菜单
-			},
+			showPopup(){this.popupShow=true;},		//显示菜单
+			hidePopup(){this.popupShow=false;},		//隐藏菜单
 			getData(){
 				setTimeout(()=> {
 					let arr = [
@@ -133,28 +163,18 @@
 							time:"10:21",
 							data:"我是第二条信息",
 							noreadnum:5
-						},
-						{
-							userpic:"../../static/demo/userpic/12.jpg",
-							username:"下拉刷新3",
-							time:"10:21",
-							data:"我是第三条信息",
-							noreadnum:9
 						}
 					];
 					this.list = arr;
 					uni.stopPullDownRefresh();
 				}, 2000);
 			},
-			//上拉加载更多
+			//上拉加载更多：如果不是"上拉加载"，证明已经在请求数据，所以直接return
 			loadmore(){
-				//如果不是"上拉加载"，证明已经在请求数据，所以直接return
 				if(this.loadtext != "上拉加载更多..."){return;}
-				//修改状态
 				this.loadtext = "加载中...";
 				//模拟获取数据
 				setTimeout(()=>{
-					//获取到数据
 					let obj={
 						userpic:"../../static/demo/userpic/12.jpg",
 						username:"上拉继续加载",
@@ -166,7 +186,11 @@
 					this.loadtext = "上拉加载更多..."	//获取数据完成
 				},1000);
 				//this.tablist[this.tabIndex].loadtext = "没有更多数据了...";
-			}
+			},
+			//点击切换子页面
+			tabChange(index){this.tabIndex = index;},
+			//滑动切换子页面
+			slideChange(e){this.tabIndex = e.detail.current ;}
 		}
 	}
 </script>
