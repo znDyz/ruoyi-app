b9ac0b0bb2cdfcd82a93fac3f5316bbc603fd87e
dongyunzhao <32562424@qq.com>
2020-03-24 20:17:37 +0800

    修改

--- ruoyi/src/main/java/com/ruoyi/mobileAPI/token/MobileToken.java
+++ ruoyi/src/main/java/com/ruoyi/mobileAPI/token/MobileToken.java
@@ -1,21 +1,12 @@
 package com.ruoyi.mobileAPI.token;
 
-import com.ruoyi.common.constant.Constants;
-import com.ruoyi.common.constant.UserConstants;
-import com.ruoyi.common.utils.SecurityUtils;
-import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.framework.security.service.TokenService;
 import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.project.system.domain.SysUser;
-import com.ruoyi.project.system.service.ISysPostService;
-import com.ruoyi.project.system.service.ISysRoleService;
 import com.ruoyi.project.system.service.ISysUserService;
-import com.sun.xml.internal.bind.v2.TODO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
