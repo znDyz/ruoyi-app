f46626ef31256d8f18d771894758cf61f4da191d
znDyz <32562424@qq.com>
2020-03-24 14:36:53 +0800

    修改登陆策略

--- myFirstApp/App.vue
+++ myFirstApp/App.vue
@@ -3,26 +3,23 @@
 		onLaunch: function() {	//app打开时调用
 			this.lib.NetWork.On();			//网路监听（用户目前断网，切换wifi）
 		},
-		onShow: function() {	//app前台运行时调用
+		onShow: function() {	
+			//app前台运行时调用，app打开验证token，如果验证通过直接跳转index页面，
+			//验证未通过，清除本地token，跳转登录页面
 			var token = uni.getStorageSync("token");
-			/* if(token!="" && token!=null && token!=undefined){
-				var username = uni.getStorageSync("username");
-				var password = uni.getStorageSync("password");
+			console.log("onShow方法中获取的token="+token);
+			if(token!="" && token!=null && token!=undefined){
 				this.iGlobal.request({
 				    url:'/checkToken', method:'POST',
 				}).then((res)=>{
-					if(res.code=="200" && res.token){
-						uni.setStorageSync('token', res.token);
-						if(_this.rememberMe){
-							uni.setStorageSync('username', _this.username);
-							uni.setStorageSync('password', _this.password);
-						}
-						_this.toIndex();
-					} 
-				}).catch((err)=>{
-				    console.log("请求失败"+JSON.stringify(res));
-				});
-			} */
+					console.log("onshou方法返回值："+JSON.stringify(res));
+					if(res.code==200){
+						uni.reLaunch({ url: "./pages/index/index"});
+					}else{
+						uni.getStorageSync("");
+					}
+				})
+			}
 		},
 		onHide: function() {	//app后台运行时调用
 			console.log('App Hide')
