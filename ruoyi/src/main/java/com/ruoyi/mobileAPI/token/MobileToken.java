44e2fb1b4e83de4a67e431daa1a843c20a3fddf1
znDyz <32562424@qq.com>
2020-03-25 10:09:51 +0800

    添加测试方法

--- ruoyi/src/main/java/com/ruoyi/mobileAPI/token/MobileToken.java
+++ ruoyi/src/main/java/com/ruoyi/mobileAPI/token/MobileToken.java
@@ -35,8 +35,6 @@ public class MobileToken {
     public AjaxResult testMethod(HttpServletRequest request)
     {
         AjaxResult ajax = AjaxResult.success("此方法用于调用测试接口");
-        System.out.println("部署时可以删除！！！！！");
-        System.out.println("我******************");
         return ajax;
     }
 }
